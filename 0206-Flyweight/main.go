a71e06366a47ef725a3a687c469ee65e26cb16c8
jansonlv <jansonlv@qq.com>
2022-06-09 16:44:00 +0800

    享元模式

---
+++ 0206-Flyweight/main.go
@@ -0,0 +1,135 @@
+package main
+
+import "fmt"
+
+type dress interface {
+	getColor() string
+}
+
+type terroristDress struct {
+	color string
+}
+
+func (t *terroristDress) getColor() string {
+	return t.color
+}
+
+func newTerroristDress() *terroristDress {
+	return &terroristDress{color: "red"}
+}
+
+type counterTerroristDress struct {
+	color string
+}
+
+func (c *counterTerroristDress) getColor() string {
+	return c.color
+}
+
+func newCounterTerroristDress() *counterTerroristDress {
+	return &counterTerroristDress{color: "green"}
+}
+
+const (
+	//TerroristDressType terrorist dress type
+	TerroristDressType = "tDress"
+	//CounterTerrroristDressType terrorist dress type
+	CounterTerrroristDressType = "ctDress"
+)
+
+var (
+	dressFactorySingleInstance = &dressFactory{
+		dressMap: make(map[string]dress),
+	}
+)
+
+type dressFactory struct {
+	dressMap map[string]dress
+}
+
+func (d *dressFactory) getDressByType(dressType string) (dress, error) {
+	// 理解为就是重用对象，当这个类别不存在就去new一个
+	if d.dressMap[dressType] != nil {
+		return d.dressMap[dressType], nil
+	}
+
+	if dressType == TerroristDressType {
+		d.dressMap[dressType] = newTerroristDress()
+		return d.dressMap[dressType], nil
+	}
+	if dressType == CounterTerrroristDressType {
+		d.dressMap[dressType] = newCounterTerroristDress()
+		return d.dressMap[dressType], nil
+	}
+
+	return nil, fmt.Errorf("Wrong dress type passed")
+}
+
+func getDressFactorySingleInstance() *dressFactory {
+	return dressFactorySingleInstance
+}
+
+type player struct {
+	dress      dress
+	playerType string
+	lat        int
+	long       int
+}
+
+func newPlayer(playerType, dressType string) *player {
+	dress, _ := getDressFactorySingleInstance().getDressByType(dressType)
+	return &player{
+		playerType: playerType,
+		dress:      dress,
+	}
+}
+
+func (p *player) newLocation(lat, long int) {
+	p.lat = lat
+	p.long = long
+}
+
+type game struct {
+	terrorists        []*player
+	counterTerrorists []*player
+}
+
+func newGame() *game {
+	return &game{
+		terrorists:        make([]*player, 0, 1),
+		counterTerrorists: make([]*player, 0, 1),
+	}
+}
+
+// 增加友军
+func (c *game) addTerrorist(dressType string) {
+	player := newPlayer("T", dressType)
+	c.terrorists = append(c.terrorists, player)
+}
+
+// 增加敌军
+func (c *game) addCounterTerrorist(dressType string) {
+	player := newPlayer("CT", dressType)
+	c.counterTerrorists = append(c.counterTerrorists, player)
+}
+
+func main() {
+	game := newGame()
+
+	//Add Terrorist
+	game.addTerrorist(TerroristDressType)
+	game.addTerrorist(TerroristDressType)
+	game.addTerrorist(TerroristDressType)
+	game.addTerrorist(TerroristDressType)
+
+	//Add CounterTerrorist
+	game.addCounterTerrorist(CounterTerrroristDressType)
+	game.addCounterTerrorist(CounterTerrroristDressType)
+	game.addCounterTerrorist(CounterTerrroristDressType)
+
+	dressFactoryInstance := getDressFactorySingleInstance()
+
+	for dressType, dress := range dressFactoryInstance.dressMap {
+		fmt.Printf("DressColorType: %s\nDressColor: %s\n", dressType, dress.getColor())
+	}
+}
