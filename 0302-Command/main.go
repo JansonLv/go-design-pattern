cbff1f319e0fae16262e8c5ee7955fc8065887b5
jansonlv <jansonlv@qq.com>
2022-06-10 16:55:54 +0800

    命令模式

---
+++ 0302-Command/main.go
@@ -0,0 +1,77 @@
+package main
+
+import "fmt"
+
+// 命令接口
+type command interface {
+	execute()
+}
+
+// 接收者接口
+type device interface {
+	on()
+	off()
+}
+
+//  请求者
+type button struct {
+	command command
+}
+
+func (b *button) press() {
+	b.command.execute()
+}
+
+//  请求者接口
+type onCommand struct {
+	device device
+}
+
+func (c *onCommand) execute() {
+	c.device.on()
+}
+
+type offCommand struct {
+	device device
+}
+
+func (c *offCommand) execute() {
+	c.device.off()
+}
+
+// 具体接收者
+type tv struct {
+	isRunning bool
+}
+
+func (t *tv) on() {
+	t.isRunning = true
+	fmt.Println("Turning tv on")
+}
+
+func (t *tv) off() {
+	t.isRunning = false
+	fmt.Println("Turning tv off")
+}
+
+func main() {
+	tv := &tv{}
+
+	onCommand := &onCommand{
+		device: tv,
+	}
+
+	offCommand := &offCommand{
+		device: tv,
+	}
+
+	onButton := &button{
+		command: onCommand,
+	}
+	onButton.press()
+
+	offButton := &button{
+		command: offCommand,
+	}
+	offButton.press()
+}
